69052488db9114724f42384232af44163377bd17
Vajda Ákos <vajdaakos@alu-fon.hu>
2024-10-21 06:53:11 +0200

    project setup

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "unity.h"
+
+int solution(int A[], int N)
+{
+
+}
+
+
+
+void setUp(void) 
+{
+    printf("\nUnity Test started.\n");
+}
+
+void tearDown(void) 
+{
+    printf("\nUnity Test ended.\n");
+}
+void odd_occurrences_in_array(void) {
+
+
+
+}
+
+
+
+int main()
+{
+
+    UNITY_BEGIN();
+    RUN_TEST(odd_occurrences_in_array);
+    return UNITY_END();
+}
