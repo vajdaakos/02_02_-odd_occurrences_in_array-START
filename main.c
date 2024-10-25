101184fd6b8d792d2d5a98623ac12ce95d556194
Vajda Ákos <vajdaakos@alu-fon.hu>
2024-10-25 05:20:30 +0200

    implemented and tested

--- main.c
+++ main.c
@@ -5,6 +5,12 @@
 
 int solution(int A[], int N)
 {
+    int res = A[0];
+    for(int i=1; i<N;++i)
+    {
+        res^=A[i];
+    }
+    return res;
 
 }
 
@@ -19,13 +25,11 @@ void tearDown(void)
 {
     printf("\nUnity Test ended.\n");
 }
-void odd_occurrences_in_array(void) {
-
-
-
+void odd_occurrences_in_array(void) 
+{
+    int A[] = {1,1,2,2,3,3,3};
+    TEST_ASSERT_EQUAL_INT( 3, solution(A,7));
 }
-
-
 
 int main()
 {
